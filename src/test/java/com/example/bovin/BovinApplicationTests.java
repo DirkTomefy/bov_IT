3b32a5d46bef15c55c68f5a88c949407418e369a
DirkTomefy <DirkTomefy@gmail.com>
2026-06-10 15:33:33 +0300

    [init]

---
+++ src/test/java/com/example/bovin/BovinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bovin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BovinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
