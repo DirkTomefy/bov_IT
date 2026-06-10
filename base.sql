69e2497bbee85a31d02b322d577b636f4f6ec893
andriamiara <f5633830@gmail.com>
2026-06-10 20:33:26 +0300

    lot: script sql

--- base.sql
+++ base.sql
@@ -4,3 +4,15 @@ CREATE TABLE race (
     id SERIAL PRIMARY KEY,
     libelle VARCHAR(100) NOT NULL
 );
+
+
+CREATE TABLE lot (
+    id SERIAL PRIMARY KEY,
+    idRace INTEGER NOT NULL,
+    nombre INTEGER NOT NULL,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_lot_race
+        FOREIGN KEY (idRace)
+        REFERENCES race(id)
+);
